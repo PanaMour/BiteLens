888006b77069e9588fd12d6a83acd7bdfb4acf07
PanaMour <60044131+PanaMour@users.noreply.github.com>
2023-04-13 14:32:07 +0300

    Added Firebase Authentication for Login and SIgnup

--- app/src/main/java/com/example/bitelens/LoginActivity.java
+++ app/src/main/java/com/example/bitelens/LoginActivity.java
@@ -1,15 +1,22 @@
 package com.example.bitelens;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,6 +24,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText passwordEditText;
     private Button loginButton;
     private TextView signUpTextView;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +36,7 @@ public class LoginActivity extends AppCompatActivity {
         passwordEditText = findViewById(R.id.password);
         loginButton = findViewById(R.id.login_button);
         signUpTextView = findViewById(R.id.signup_link);
-
+        mAuth = FirebaseAuth.getInstance();
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,18 +53,46 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    private boolean isValidEmail(String email) {
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        return email.matches(emailPattern);
+    }
+
     private void loginUser() {
         String email = emailEditText.getText().toString().trim();
         String password = passwordEditText.getText().toString().trim();
 
-        // TODO: Implement the login logic with Firebase Authentication
+        if (TextUtils.isEmpty(email)) {
+            emailEditText.setError("Email is required.");
+            return;
+        }
 
-        // For now, we'll show a toast message saying "Login Successful"
-        Toast.makeText(this, "Login Successful", Toast.LENGTH_SHORT).show();
+        if (!isValidEmail(email)) {
+            emailEditText.setError("Invalid email format.");
+            return;
+        }
 
-        // After successful login, navigate to MainActivity
-        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        if (TextUtils.isEmpty(password)) {
+            passwordEditText.setError("Password is required.");
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success
+                            Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user
+                            Toast.makeText(LoginActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
+
 }
--- app/src/main/java/com/example/bitelens/SignupActivity.java
+++ app/src/main/java/com/example/bitelens/SignupActivity.java
@@ -1,15 +1,22 @@
 package com.example.bitelens;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -18,6 +25,7 @@ public class SignupActivity extends AppCompatActivity {
     private EditText confirmPasswordEditText;
     private Button signUpButton;
     private TextView loginTextView;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +38,7 @@ public class SignupActivity extends AppCompatActivity {
         confirmPasswordEditText = findViewById(R.id.confirm_password);
         signUpButton = findViewById(R.id.signup_button);
         loginTextView = findViewById(R.id.login_link);
-
+        mAuth = FirebaseAuth.getInstance();
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,20 +54,58 @@ public class SignupActivity extends AppCompatActivity {
             }
         });
     }
+    private boolean isValidEmail(String email) {
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        return email.matches(emailPattern);
+    }
 
     private void signUpUser() {
         String email = emailEditText.getText().toString().trim();
         String password = passwordEditText.getText().toString().trim();
         String confirmPassword = confirmPasswordEditText.getText().toString().trim();
 
-        // TODO: Implement the sign-up logic with Firebase Authentication
+        if (TextUtils.isEmpty(email)) {
+            emailEditText.setError("Email is required.");
+            return;
+        }
 
-        // For now, we'll show a toast message saying "Sign Up Successful"
-        Toast.makeText(this, "Sign Up Successful", Toast.LENGTH_SHORT).show();
+        if (!isValidEmail(email)) {
+            emailEditText.setError("Invalid email format.");
+            return;
+        }
 
-        // After successful sign-up, navigate to LoginActivity
-        Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
-        startActivity(intent);
-        finish();
+        if (TextUtils.isEmpty(password)) {
+            passwordEditText.setError("Password is required.");
+            return;
+        }
+
+        if (TextUtils.isEmpty(confirmPassword)) {
+            confirmPasswordEditText.setError("Password confirmation is required.");
+            return;
+        }
+
+        if (!password.equals(confirmPassword)) {
+            confirmPasswordEditText.setError("Passwords do not match.");
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign up success
+                            Toast.makeText(SignupActivity.this, "Sign Up Successful", Toast.LENGTH_SHORT).show();
+                            // Navigate to LoginActivity
+                            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // If sign up fails, display a message to the user
+                            Toast.makeText(SignupActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
+
 }
