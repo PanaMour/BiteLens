c4100ec00ac9894b134505b7a378580ba66aef71
PanaMour <60044131+PanaMour@users.noreply.github.com>
2023-05-30 11:55:43 +0300

    Added BarGraphView

---
+++ app/src/main/java/com/example/bitelens/BarGraphView.java
@@ -0,0 +1,59 @@
+package com.example.bitelens;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.View;
+
+import java.util.Collections;
+import java.util.List;
+
+public class BarGraphView extends View {
+    private Paint paint;
+    private List<Integer> data;
+    private int maxValue;
+
+    public BarGraphView(Context context, List<Integer> data) {
+        super(context);
+        this.data = data;
+        paint = new Paint();
+        maxValue = Collections.max(data);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (data != null) {
+            int width = getWidth() / data.size();  // The width of each bar
+
+            // Set the paint color for drawing the axis
+            paint.setColor(android.graphics.Color.BLACK);
+            paint.setStrokeWidth(2);
+            paint.setTextSize(30); // Set a text size for the labels.
+
+            // Draw y-axis, note that it starts at getHeight() - 200 now, not at 0.
+            canvas.drawLine(100, getHeight() - 200, 100, 0, paint);
+
+            // Draw x-axis
+            canvas.drawLine(100, getHeight() - 200, getWidth(), getHeight() - 200, paint);
+
+            // Draw y-axis labels
+            int labelHeight = (getHeight() - 200) / (maxValue / 500);
+            for (int i = 500; i <= maxValue; i += 500) {
+                int textHeight = (int) (getHeight() - 200 - ((i / (float) maxValue) * (getHeight() - 200)));
+                canvas.drawText(String.valueOf(i), 30, textHeight + 15, paint); // Adjusted x-coordinate from 50 to 30
+            }
+
+            // Set the paint color for drawing the bars
+            paint.setColor(android.graphics.Color.BLUE);
+
+            for (int i = 0; i < data.size(); i++) {
+                int height = (getHeight() - 200) * data.get(i) / maxValue;  // The height of the bar
+                canvas.drawRect(100 + (i * width), getHeight() - height - 200, 100 + ((i + 1) * width), getHeight() - 200, paint);
+            }
+        }
+    }
+
+
+
+}
--- app/src/main/java/com/example/bitelens/StatisticsActivity.java
+++ app/src/main/java/com/example/bitelens/StatisticsActivity.java
@@ -3,6 +3,7 @@ package com.example.bitelens;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -11,9 +12,15 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class StatisticsActivity extends AppCompatActivity {
 
@@ -24,7 +31,7 @@ public class StatisticsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
-
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
@@ -42,6 +49,38 @@ public class StatisticsActivity extends AppCompatActivity {
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         setupDrawerContent(navigationView);
+
+        List<Integer> data = new ArrayList<>();
+        data.add(2000);
+        data.add(2500);
+        data.add(2300);
+        data.add(1800);
+        data.add(2400);
+        data.add(2000);
+        data.add(2300);
+
+        int totalValues = 30;
+        int zeroPercentage = 10;
+        int zeroCount = (int) Math.round(totalValues * zeroPercentage / 100.0);
+
+        Random random = new Random();
+
+        for (int i = 0; i < totalValues - zeroCount; i++) {
+            int randomValue = random.nextInt(1000) + 1800; // Generate random values between 1800 and 2800
+            data.add(randomValue);
+        }
+
+        for (int i = 0; i < zeroCount; i++) {
+            data.add(0);
+        }
+        LinearLayout layout = findViewById(R.id.barGraphContainer);
+        BarGraphView graph = new BarGraphView(this, data);
+
+        // Set custom width and height
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, 750);
+        graph.setLayoutParams(params);
+
+        layout.addView(graph);
     }
 
     private void setupDrawerContent(NavigationView navigationView) {
